2f058b469bf459d91813222dfca9a74c955860b8
Callee84 <carl.g.holm@gmail.com>
2022-11-22 17:51:13 +0000

    Add new page for posting blog entries to be displayed on guestbook-page

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from .views import PostGuestView, AddGuestPost
 
 
 urlpatterns = [
-    path('', views.PostGuestView.as_view(), name='guestbook')
+    path('', views.PostGuestView.as_view(), name='guestbook'),
+    path('add_post/', AddGuestPost.as_view(), name='add_guest_post'),
 ]
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import PostGuest
 # Create your views here.
 
@@ -11,3 +11,9 @@ class PostGuestView(ListView):
     model = PostGuest
     template_name = 'guestbook.html'
     paginate_by = 10
+
+
+class AddGuestPost(CreateView):
+    model = PostGuest
+    template_name = 'add_guest_post.html'
+    fields = '__all__'
