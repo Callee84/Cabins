4a18826778e72fdc70f39504a1bd2d5c6943de15
Callee84 <carl.g.holm@gmail.com>
2022-11-25 12:29:29 +0000

    Start on reservation model

--- cabins_reservations/models.py
+++ cabins_reservations/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+cabin_choice = (
+        ('SAL', 'Cabin Sälen'),
+        ('OLA', 'Öland'),
+)
+
+
+class Guest(models.Model):
+    guest_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=25, required=True)
+    last_name = models.CharField(max_length=35, required=True)
+    email = models.EmailField(required=True)
+    phone_nr = PhoneNumberField(null=True)
+
+    def __str__(self):
+        return self.last_name + ', ' + self.first_name
+
+
+class Booking(models.Model):
+
+    booking_nr = models.AutoField(primary_key=True)
+    guest = models.ForeignKey(
+            Guest, on_delete=models.CASCADE, related_name=guest)
+    cabin = models.CharField(choices=cabin_choice, null=False)
+    nr_of_guests = ((1, '1 Guest'), (2, '2 Guests'), (3, '3 Guests'),
+                    (4, '4 Guests'), (5, '5 Guests'), (6, '6 Guests'),
+                    (7, '2 Guests'), (8, '2 Guests'), (9, '9 Guests'),
+                    (10, '2 Guests'), (11, '2 Guests'), (12, '12 Guests'))
+    guests = models.IntegerField(choices=nr_of_guests, default=2)
+    arrival = models.DateField()
+    departure = models.DateField()
