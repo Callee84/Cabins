1cc6491126e829d860af2f06179f696e9251c2c1
Callee84 <carl.g.holm@gmail.com>
2022-11-29 17:50:04 +0000

    Adding more style to site. Added img on news, guestbook and addning posts

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,28 @@
+/**
+ * script for the countdown
+ */
 
+ const countdown = () => {
+    const countDate = new Date ("December 2, 2022 09:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    // counting time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // calculate day
+    const showDay = Math.floor(gap / day);
+    const showHour = Math.floor ((gap % day) / hour)
+    const showMin = Math.floor ((gap % hour) / minute)
+    const showSec = Math.floor ((gap % minute) / second)
+
+    document.querySelector(".day").innerText = showDay;
+    document.querySelector(".hour").innerText = showHour;
+    document.querySelector(".min").innerText = showMin;
+    document.querySelector(".secs").innerText = showSec;
+};
+
+setInterval(countdown, 1000)
